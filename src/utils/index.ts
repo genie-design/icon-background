79848c97155edcce37c28465050a03792eee74f0
Corey Jepperson <corey.jepperson@gmail.com>
2024-02-23 11:57:53 -0600

    add fallback colors

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,35 @@
 export const squareIntersectsCircle = (
-  circleX: number,
-  circleY: number,
-  radius: number,
-  squareX: number,
-  squareY: number,
-  squareWidth: number
+	circleX: number,
+	circleY: number,
+	radius: number,
+	squareX: number,
+	squareY: number,
+	squareWidth: number
 ): boolean => {
-  // Calculate the distance between the center of the circle and each corner of the square
-  const dx = Math.max(Math.abs(circleX - squareX) - squareWidth / 2, 0);
-  const dy = Math.max(Math.abs(circleY - squareY) - squareWidth / 2, 0);
-  const distance = Math.sqrt(dx * dx + dy * dy);
+	// Calculate the distance between the center of the circle and each corner of the square
+	const dx = Math.max(Math.abs(circleX - squareX) - squareWidth / 2, 0);
+	const dy = Math.max(Math.abs(circleY - squareY) - squareWidth / 2, 0);
+	const distance = Math.sqrt(dx * dx + dy * dy);
 
-  // If the distance is less than or equal to the radius of the circle, the square intersects the circle
-  return distance <= radius;
+	// If the distance is less than or equal to the radius of the circle, the square intersects the circle
+	return distance <= radius;
 };
 export function cssStringify(obj: Partial<CSSStyleDeclaration>) {
-  return Object.keys(obj).map(k => `${k}: ${obj[k as keyof CSSStyleDeclaration]}`).join(';')
+	return Object.keys(obj)
+		.map((k) => `${k}: ${obj[k as keyof CSSStyleDeclaration]}`)
+		.join(';');
 }
+export const log = (...text: Parameters<typeof console.log>) => {
+	text = text.map((t) => {
+		if (typeof t === 'object') {
+			try {
+				return JSON.stringify(t, null, 2);
+			} catch {
+				return t.toString();
+			}
+		}
+		return t;
+	});
+	console.log(...text);
+	console.log('-----------------------------------------------');
+};
